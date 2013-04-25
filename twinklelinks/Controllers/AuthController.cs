08420dc38fec5ac66dacd53b168c8542763b1a7e
Ryan McDonnell <ryan@ryanmcdonnell.com>
2013-04-24 22:30:41 -0700

    Authenticate and pull tweets using TweetSharp

---
+++ twinklelinks/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TweetSharp;
+
+namespace twinklelinks.Controllers
+{
+    public class AuthController : Controller
+    {
+        // Based on samples taken from https://github.com/danielcrenna/tweetsharp
+
+        public ActionResult Authorize()
+        {
+            // Step 1 - Retrieve an OAuth Request Token
+            TwitterService service = new TwitterService(ConfigurationManager.AppSettings["TwitterConsumerKey"], ConfigurationManager.AppSettings["TwitterConsumerSecret"]);
+
+            // This is the registered callback URL
+            OAuthRequestToken requestToken = service.GetRequestToken(ConfigurationManager.AppSettings["TwitterCallbackUrl"]);
+
+            // Step 2 - Redirect to the OAuth Authorization URL
+            Uri uri = service.GetAuthorizationUri(requestToken);
+            return new RedirectResult(uri.ToString(), false /*permanent*/);
+        }
+
+        // This URL is registered as the application's callback at http://dev.twitter.com
+        public ActionResult AuthorizeCallback(string oauth_token, string oauth_verifier)
+        {
+            var requestToken = new OAuthRequestToken { Token = oauth_token, OAuthCallbackConfirmed = true };
+
+            TwitterService service = new TwitterService(ConfigurationManager.AppSettings["TwitterConsumerKey"], ConfigurationManager.AppSettings["TwitterConsumerSecret"]);
+            OAuthAccessToken accessToken = service.GetAccessToken(requestToken, oauth_verifier.ToString());
+
+            Session["Twitter.AccessToken"] = accessToken.Token;
+            Session["Twitter.AccessTokenSecret"] = accessToken.TokenSecret;
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ twinklelinks/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TweetSharp;
+
+namespace twinklelinks.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var token = Session["Twitter.AccessToken"];
+            var tokenSecret = Session["Twitter.AccessTokenSecret"];
+
+            if (token == null || tokenSecret == null)
+                return RedirectToAction("Authorize", "Auth");
+
+            TwitterService service = new TwitterService(ConfigurationManager.AppSettings["TwitterConsumerKey"], ConfigurationManager.AppSettings["TwitterConsumerSecret"]);
+            service.AuthenticateWith(token.ToString(), tokenSecret.ToString());
+            
+            // TODO: Check that authentication succeeded
+
+            TwitterUser user = service.VerifyCredentials(new VerifyCredentialsOptions());
+            ViewBag.TwitterUsername = user.ScreenName;
+
+            IEnumerable<TwitterStatus> favorites = service.ListFavoriteTweets(new ListFavoriteTweetsOptions { UserId = user.Id, Count = 20, IncludeEntities = true });
+            ViewBag.Favorites = favorites;
+
+            return View();
+        }
+    }
+}
